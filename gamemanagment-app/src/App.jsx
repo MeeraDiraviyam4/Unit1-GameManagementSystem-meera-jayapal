fddd6e9e5a7be382d42a33ad3866262dadf9ef29
meera jayapal <maildira@gmail.com>
2026-03-11 10:10:29 -0500

    Initial React setup with Vite and react-router

---
+++ gamemanagment-app/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="hello-text">Hello, LaunchCode</h1>
+    </div>
+  );
+}
+
+export default App;
