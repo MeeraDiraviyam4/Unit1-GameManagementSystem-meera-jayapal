bf1d1487c184c2ffbc7b6de76f050d554b76a489
meera jayapal <maildira@gmail.com>
2026-04-01 16:03:52 -0500

    created Organizer Dashboard

--- gamemanagment-app/src/App.jsx
+++ gamemanagment-app/src/App.jsx
@@ -1,10 +1,10 @@
-
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Layout/Header.jsx";
 import Footer from "./components/Layout/Footer.jsx";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import PlayerDashboard from "./components/Player/PlayerDashboard.jsx";
+import OrganizerDashboard from "./components/Organizer/OrganizerDAshboard.jsx"
 
 function App() {
   return (
@@ -12,15 +12,15 @@ function App() {
       <Header />
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/"element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/player" element={<PlayerDashboard />} />
+          <Route path="/organizer" element={<OrganizerDashboard />} />
         </Routes>
       </main>
       <Footer />
     </div>
   );
 }
-
 
 export default App;
